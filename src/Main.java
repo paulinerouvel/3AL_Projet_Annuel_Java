ac8d6c86ba9ad23bb1d6a4df73f700c86f10283b
Grégory <gregory.brugnet@gmail.com>
2019-04-25 10:02:38 +0200

    Repo initial pour la partie java

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import Annotations.Documentation;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import Annotations.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Hello World");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        Text scenetitle = new Text("Bienvenue sur WasteMart");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+        Label userName = new Label("Login");
+        grid.add(userName, 0, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+        Label pw = new Label("Mot de passe");
+        grid.add(pw, 0, 2);
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+        Button btn = new Button("Se connecter");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        primaryStage.setMaximized(true); // met en plein écran au lancement
+
+
+        Scene scene = new Scene(grid, primaryStage.getMaxWidth(), primaryStage.getMaxHeight() ); // crée la scène
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,12 @@
+public class User {
+    private Integer id;
+    private String libelle;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String tel;
+    private String adresse;
+    private String ville;
+    private String codePostal;
+    
+}
